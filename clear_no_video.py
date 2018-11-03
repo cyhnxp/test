42b789033aaa52ce8d340a98a9ae7561429dcfe4
yuhao <328863397@qq.com>
2018-11-03 16:54:31 +0800

    init

--- clear_no_video.py
+++ clear_no_video.py
@@ -1,6 +1,22 @@
 import os
 import shutil
 import threading
+import cv2
+
+
+picture_path='video'
+
+
+def video2frame(video_src,save_path):
+    cap = cv2.VideoCapture(video_src)
+    count=cap.get(cv2.CAP_PROP_FRAME_COUNT)
+
+    interval=int(count/20)+1
+
+    for i in range(0,int(count),interval):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, i)
+        a, b = cap.read()
+        cv2.imwrite(save_path+os.sep+'%d.jpg' % int(i/interval), b)
 
 
 def delete_dirs(video_src_path):
@@ -10,15 +26,24 @@ def delete_dirs(video_src_path):
             dir_path=video_src_path+os.sep+dir
             files=os.listdir(dir_path)
 
+            video_path=''
             for file in files:
                 suffix_name=os.path.splitext(file)[1] # 获取文件后缀名
                 is_delete=True
                 if suffix_name in video_formats:
                     is_delete=False
+                    video_path=file
                     break
             if is_delete:
                 print('deleted:',dir_path)
                 shutil.rmtree(dir_path)
+            else:
+                # 处理视频
+                pic=dir_path + os.sep + picture_path
+                if not os.path.exists(pic):
+                    os.makedirs(pic)
+                video2frame(dir_path+os.sep+video_path,pic)
+
             is_delete=True
         except:
             pass
--- test.py
+++ test.py
@@ -1,5 +1,18 @@
+import cv2
+import os
 
-vie=['.mp4','.mp3']
 
-if '.mp4' in vie:
-    print("dfdsf")
+def video2frame(video_src,save_path):
+    cap = cv2.VideoCapture(video_src)
+    count=cap.get(cv2.CAP_PROP_FRAME_COUNT)
+
+    interval=int(count/20)+1
+
+    for i in range(0,int(count),interval):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, i)
+        a, b = cap.read()
+        cv2.imwrite(save_path+os.sep+'%d.jpg' % int(i/interval), b)
+
+
+if __name__ == '__main__':
+    video2frame('0.mp4','pic')
